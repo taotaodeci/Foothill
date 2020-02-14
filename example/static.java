24b8268e4270f5dfc13be14ab05fb8d64ee81824
taotaodeci <59940385+taotaodeci@users.noreply.github.com>
2020-02-14 13:55:34 -0800

    Update static.java

--- example/static.java
+++ example/static.java
@@ -7,9 +7,9 @@ public class HelloWorld{
         System.out.println(a.value);
         System.out.println(b.value);
         a.value = 1;
-        System.out.println(MyClass.value);
+        System.out.println(MyClass.value);  // also changed
         System.out.println(a.value);
-        System.out.println(b.value);
+        System.out.println(b.value);  // also changed
 /* Output is
 Hello World
 0
@@ -19,9 +19,29 @@ Hello World
 1
 1
 */      
+        // System.out.println(MyClass.nonStaticValue);  WRONG, Can't access non-static variable
+        System.out.println(a.nonStaticValue);  // 1
+        System.out.println(b.nonStaticValue);  // 1
+        a.nonStaticValue = 2;
+        System.out.println(a.nonStaticValue);  // 2
+        System.out.println(b.nonStaticValue);  // 1 not changed
      }
 }
 
 class MyClass {
    static int value = 0;
+   int nonStaticValue = 1;
+     
+     static boolean valueIsZero() {
+          // return nonStaticValue;   WRONG, CAN't access non-static variable.
+          return value == 0;
+     }
+     
+     boolean nonStaticValueIsZero() {
+          return nonStaticValue == 0;
+     }
+     
+     boolean valueEqual() {
+          return value == nonStaticValue;  // OK, can access static variable
+     }
 }
